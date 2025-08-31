d4d23f74548e86d75fbcc2b608ea75080a6d884c
Zach Waterfield <zlwaterfield@gmail.com>
2025-08-31 15:15:29 -0400

    Remove old config

--- app/src/config/database.config.ts
+++ app/src/config/database.config.ts
@@ -2,6 +2,4 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
   url: process.env.DATABASE_URL,
-  supabaseUrl: process.env.SUPABASE_URL,
-  supabaseKey: process.env.SUPABASE_KEY,
 }));
